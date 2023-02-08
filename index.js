560d584d381549afaa8c6d166499908e39e71f4b
Ahammed Fahim Galib <galib0124gmail.com>
2023-02-08 18:39:01 +0600

    specifiq id aquire

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWARD}@cluster0.w7wfspi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 const run = async() => {
     try{
+        const alldata = client.db('rent-sell-server').collection('rentandsell');
 
         app.get('/property', async(req, res) => {
             const query = {};
-            const allservice = await allCategori.find(query).toArray();
-            res.send(allservice);
+            const allflat = await alldata.find(query).toArray();
+            res.send(allflat);
+        })
+        app.get('/property/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const flat = await alldata.find(query).toArray();
+            res.send(flat);
         })
         
     }finally{
